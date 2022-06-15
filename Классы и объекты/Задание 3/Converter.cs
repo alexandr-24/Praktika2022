c524ec370c6c6a2e86d1af4a6542d9a28b6d4096
Александр Кабыткин <akab2412@gmail.com>
2022-06-15 11:43:50 +0300

    Выполнено задание 3

---
+++ Классы и объекты/Задание 3/Converter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    public class Converter
+    {
+        private double usd;
+        private double eur;
+        private double rub;
+
+        public Converter(double USD, double EUR, double RUB)
+        {
+            usd = USD;
+            eur = EUR;
+            rub = RUB;
+        }
+
+        public double ConvertToUSD(double UAH)
+        {
+            return Math.Round(UAH / usd, 2);
+        }
+        public double ConvertFromUSD(double USD)
+        {
+            return Math.Round(USD * usd, 2);
+        }
+
+        public double ConvertToEUR(double UAH)
+        {
+            return Math.Round(UAH / eur, 2);
+        }
+        public double ConvertFromEUR(double EUR)
+        {
+            return Math.Round(EUR * eur, 2);
+        }
+
+        public double ConvertToRUB(double UAH)
+        {
+            return Math.Round(UAH / rub, 2);
+        }
+        public double ConvertFromRUB(double RUB)
+        {
+            return Math.Round(RUB * rub, 2);
+        }
+    }
+}
--- Классы и объекты/Задание 3/Program.cs
+++ Классы и объекты/Задание 3/Program.cs
@@ -10,6 +10,17 @@ namespace Задание_3
     {
         static void Main(string[] args)
         {
+            Converter myConverter = new Converter(29.55, 30.96, 0.52);
+            Console.WriteLine("1000 UAH: ");
+            Console.WriteLine(myConverter.ConvertToUSD(1000) + " USD");
+            Console.WriteLine(myConverter.ConvertToEUR(1000) + " EUR");
+            Console.WriteLine(myConverter.ConvertToRUB(1000) + " RUB");
+
+            Console.WriteLine("\n1000 USD = " + myConverter.ConvertFromUSD(1000) + " UAH");
+            Console.WriteLine("1000 EUR = " + myConverter.ConvertFromEUR(1000) + " UAH");
+            Console.WriteLine("1000 RUB = " + myConverter.ConvertFromRUB(1000) + " UAH");
+
+            Console.ReadKey();
         }
     }
 }
