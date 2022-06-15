8aa742a7e4cfc368f231395e2a16c7567fae0ec7
Александр Кабыткин <akab2412@gmail.com>
2022-06-15 10:15:34 +0300

    Выполнено задание 1

--- Классы и объекты/Задание 1/Program.cs
+++ Классы и объекты/Задание 1/Program.cs
@@ -10,6 +10,20 @@ namespace Задание_1
     {
         static void Main(string[] args)
         {
+            double s1;
+            double s2;
+
+            Console.Write("Введите 1-ую сторону прямоугольника: ");
+            s1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите 2-ую сторону прямоугольника: ");
+            s2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle myRectangle = new Rectangle(s1, s2);
+
+            Console.WriteLine("Площадь прямоугольника: " + myRectangle.Area);
+            Console.WriteLine("Периметр прямоугольника: " + myRectangle.Perimeter);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Классы и объекты/Задание 1/Rectangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    public class Rectangle
+    {
+        private double side1; // 1-ая сторона прямоугольника
+        private double side2; // 2-ая сторона прямоугольника
+
+        /// <summary>
+        /// Констуктор прямоугольника
+        /// </summary>
+        /// <param name="s1">1-ая сторона</param>
+        /// <param name="s2">2-ая сторона</param>
+        public Rectangle(double s1, double s2)
+        {
+            side1 = s1;
+            side2 = s2;
+        }
+
+        /// <summary>
+        /// Площадь прямоугольника
+        /// </summary>
+        /// <returns>Площадь прямоугольника</returns>
+        private double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        /// <summary>
+        /// Периметр прямоугольника
+        /// </summary>
+        /// <returns>Периметр прямоугольника</returns>
+        private double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+
+        /// <summary>
+        /// Площадь прямоугольника
+        /// </summary>
+        public double Area
+        {
+            get { return AreaCalculator(); }
+        }
+
+        /// <summary>
+        /// Периметр прямоугольника
+        /// </summary>
+        public double Perimeter
+        {
+            get { return PerimeterCalculator(); }
+        }
+    }
+}
