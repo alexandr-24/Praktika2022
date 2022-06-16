727307c5fbb4e8a44090f72545fab386fdf6003e
Александр Кабыткин <akab2412@gmail.com>
2022-06-16 15:19:25 +0300

    Выполнено задание 5

---
+++ Классы и объекты/Задание 5/Invoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    public class Invoice
+    {
+        public readonly int Account; // Счет
+        public readonly string Customer; // Клиент
+        public readonly string Provider; // Поставщик
+
+        private string _article; // Изделие
+        private int _quantity; // Количество
+
+        public Invoice(int account, string customer, string provider, string article, int quantity) : this(account, customer, provider)
+        {
+            _article = article;
+            _quantity = quantity;
+        }
+
+        public Invoice(int account, string customer, string provider)
+        {
+            Account = account;
+            Customer = customer;
+            Provider = provider;
+        }
+
+        public double GetAccountWithNds(double nds = 0.2)
+        {
+            return Account + (Account * nds);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Клиент: {0}\nПоставщик: {1}\nТовар: {4} ({5})\nЦена без НДС: {2}\nЦена с НДС: {3}", Customer, Provider, Account, GetAccountWithNds(), _article, _quantity);
+        }
+    }
+}
--- Классы и объекты/Задание 5/Program.cs
+++ Классы и объекты/Задание 5/Program.cs
@@ -10,7 +10,22 @@ namespace Задание_5
     {
         static void Main(string[] args)
         {
+            Console.Write("Клиент: ");
+            string client = Console.ReadLine();
+            Console.Write("Поставщик: ");
+            string postav = Console.ReadLine();
+            Console.Write("Счет: ");
+            int summa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Товар: ");
+            string tovar = Console.ReadLine();
+            Console.Write("Количество: ");
+            int k = Convert.ToInt32(Console.ReadLine());
 
+
+            Invoice invoice = new Invoice(summa, client, postav, tovar, k);
+            invoice.Show();
+
+            Console.ReadKey();
         }
     }
 }
