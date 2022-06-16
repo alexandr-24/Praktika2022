eca20d4c04ded694def474ce90cedeb5fdcf5d11
Александр Кабыткин <akab2412@gmail.com>
2022-06-16 13:38:40 +0300

    Выполнено задание 3

---
+++ Массивы и индексаторы/Задание 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+
+    public static class ArrayExt
+    {
+        public static void Sort(this int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int value = array[i];
+                        array[i] = array[j];
+                        array[j] = value;
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 1, 3, 0 };
+            a.Sort();
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
