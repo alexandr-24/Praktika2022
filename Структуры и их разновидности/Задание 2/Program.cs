20eb2632227900c2e546fc73cec89512ea01cfea
Александр Кабыткин <akab2412@gmail.com>
2022-06-17 11:37:18 +0300

    Выполнено задание 2

--- Структуры и их разновидности/Задание 2/Program.cs
+++ Структуры и их разновидности/Задание 2/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Задание_2
 {
+    struct MyStruct
+    {
+        public string Change { get; set; }
+    }
+    
+    class MyClass
+    {
+        public string Change { get; set; }
+    }
+
     internal class Program
     {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.Change = "Изменено";
+        }
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.Change = "Изменено";
+        }
+
         static void Main(string[] args)
         {
+            MyStruct s = new MyStruct();
+            s.Change = "Не изменено";
+            StruktTaker(s);
+            Console.WriteLine(s.Change);
+
+            MyClass c = new MyClass();
+            c.Change = "Не изменено";
+            ClassTaker(c);
+            Console.WriteLine(c.Change);
+
+            Console.ReadKey();
         }
     }
 }
