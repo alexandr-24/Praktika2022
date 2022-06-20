b18e6a9f6a26173994468f46405dc13cf2838b48
Александр Кабыткин <akab2412@gmail.com>
2022-06-20 15:00:39 +0300

    Выполнено задание 3

--- Методы/Задание 3/MainWindow.xaml.cs
+++ Методы/Задание 3/MainWindow.xaml.cs
@@ -24,5 +24,40 @@ namespace Задание_3
         {
             InitializeComponent();
         }
+
+        public double Calculation(double chislo1, double chislo2, int operation)
+        {
+            switch (operation)
+            {
+                case 1:
+                    return chislo1 % chislo2;
+                case 2:
+                    return Math.Pow(chislo1, chislo2);
+                case 3:
+                    return Convert.ToDouble(Convert.ToString(chislo1) + Convert.ToString(chislo2));
+                case 4:
+                    return chislo1 / chislo2;
+                default:
+                    return 0;
+            }
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int operation;
+            if (Radio1.IsChecked == true)
+                operation = 1;
+            else if (Radio2.IsChecked == true)
+                operation = 2;
+            else if (Radio3.IsChecked == true)
+                operation = 3;
+            else if (Radio4.IsChecked == true)
+                operation = 4;
+            else
+                operation = 0;
+
+
+            ResultTB.Text = "Результат: " + Calculation(Convert.ToDouble(Chislo1.Text), Convert.ToDouble(Chislo2.Text), operation);
+        }
     }
 }
